c9f194789cd8c5124901540b66ca1cbcdfe01637
SergeyBorilo <S_Barya@i.ua>
2025-01-25 11:53:46 +0200

    Initial commit

---
+++ 5Layers.Animals/src/Animals.Api/Constants/Routes.cs
@@ -0,0 +1,8 @@
+﻿namespace Animals.Api.Constants;
+
+public static class Routes
+{
+    private const string Prefix = "api";
+
+    public const string Animals = $"{Prefix}/animals";
+}
---
+++ 5Layers.Animals/src/Animals.Infrastructure/Application/Domain/Animals/Queries/GetAnimalsByName/GetAnimalsByNameQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Animals.Application.Domain.Animals.Queries.GetAnimalDetails;
+using Animals.Application.Domain.Animals.Queries.GetAnimalsByName;
+using Animals.Persistence.Core.Animals.DataProvider;
+using MediatR;
+
+namespace Animals.Infrastructure.Application.Domain.Animals.Queries.GetAnimalsByName;
+
+internal class GetAnimalsByNameQueryHandler(IAnimalsDataProvider animalsDataProvider)
+    : IRequestHandler<GetAnimalsByNameQuery, List<AnimalsByNameDto>>
+{
+    public async Task<List<AnimalsByNameDto>> Handle(GetAnimalsByNameQuery query, CancellationToken cancellationToken)
+    {
+        var animals = await animalsDataProvider.GetAll();
+
+        Debug.Assert(animals != null, nameof(animals) + " != null");
+        var result = animals
+            .Where(animal => animal.Name.Contains(query.Name))
+            .OrderBy(animal => animal.Name)
+            .Select(animal => new AnimalsByNameDto()
+            {
+                Id = animal.Id,
+                Name = animal.Name,
+                Age = animal.Age,
+                Description = animal.Description,
+            })
+            .ToList();
+
+        return result;
+
+    }
+}
